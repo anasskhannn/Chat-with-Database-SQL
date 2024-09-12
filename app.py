748f6a637cb6df539b813abf1501e40c3185eb8e
Mohd Anas Khan <anascollege2129@eng.rizvi.edu.in>
2024-09-12 11:44:18 +0530

    Update app.py in Main

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.title("🦜 LangChain: Chat with SQL DB")
 LOCALDB="USE_LOCALDB"
 MYSQL="USE_MYSQL"
 
-radio_opt=["Use SQLLite 3 Database- Student.db","Connect to you MySQL Database"]
+radio_opt=["Use SQLLite 3 Database","Connect to you MySQL Database"]
 
 selected_opt=st.sidebar.radio(label="Choose the DB which you want to chat",options=radio_opt)
 
